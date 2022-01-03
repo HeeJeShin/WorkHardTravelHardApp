89ace20c88e8de55a983b2092c355f0277c91f76
heeje0910 <shjs6711@gmail.com>
2022-01-03 15:35:55 +0900

    [WorkHardTravelHardApp]nomadcoder clone coding

---
+++ App.js
@@ -0,0 +1,158 @@
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  TextInput,
+  ScrollView,
+  Alert,
+} from "react-native";
+import { theme } from "./colos";
+import { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Fontisto } from "@expo/vector-icons";
+
+const STORAGE_KEY = "@toDos";
+
+export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+  const saveToDos = async (toSave) => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  };
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    setToDos(JSON.parse(s));
+  };
+  useEffect(() => {
+    loadToDos();
+  }, []);
+  const addToDo = async () => {
+    if (text === "") {
+      return;
+    }
+    const newToDos = Object.assign({}, toDos, {
+      ...toDos,
+      [Date.now()]: { text, working },
+    });
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    setText("");
+  };
+  const deleteToDo = (key) => {
+    Alert.alert("Delete TO Do", "Are you sure?", [
+      { text: "Cancel" },
+      {
+        text: "I'm Sure",
+        onPress: () => {
+          const newToDos = { ...toDos };
+          delete newToDos[key];
+          setToDos(newToDos);
+          saveToDos(newToDos);
+        },
+      },
+    ]);
+  };
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          {/* <TouchableWithoutFeedback> 눌렀을때 반응이 없는 버튼.. 
+          </TouchableWithoutFeedback> */}
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        {/* <TouchableHighlight
+          underlayColor="red"
+          activeOpacity={0}
+          onPress={() => console.log("pressed")}
+        >  배경을 변화시켜주는 버튼 */}
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <TextInput
+        onSubmitEditing={addToDo}
+        onChangeText={onChangeText}
+        returnKeyType="done"
+        value={text}
+        placeholder={working ? "Add a To Do" : "where do you want to go?"}
+        style={styles.input}
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View style={styles.todo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+              <TouchableOpacity onPress={() => deleteToDo(key)}>
+                <Fontisto name="trash" size={20} color="white" />
+              </TouchableOpacity>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+    // 패딩 가로로 이십?
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 80,
+  },
+  btnText: {
+    fontSize: 30,
+    fontWeight: "600",
+    color: "white",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+  todo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
---
+++ colos.js
@@ -0,0 +1,7 @@
+export const theme = {
+  bg: "black",
+  grey: "#3A3D40",
+  toDoBg: "gray",
+};
+
+//어플의 라이트 모드와 다크모드 감별기능.
